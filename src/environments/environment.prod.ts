00864667ee6d3a44cbb0d8c2db527a5159b6fc2e
Omry <omrimarebera85@gmail.com>
2020-08-23 22:58:29 +0200

    dev update
    env setting for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC__Mfi1b3TozKki4EA3mkuY9WFNUyb794",
+    authDomain: "camera-62d75.firebaseapp.com",
+    databaseURL: "https://camera-62d75.firebaseio.com",
+    projectId: "camera-62d75",
+    storageBucket: "camera-62d75.appspot.com",
+    messagingSenderId: "250454587599",
+    appId: "1:250454587599:web:c64b1e053f7d4c3084dfba",
+    measurementId: "G-B4EVZZMP9H"
+  }
 };
